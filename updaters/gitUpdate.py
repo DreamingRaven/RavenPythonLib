eb8a6a0edf8e9aa728ab2429de8604df623ed0ff
DreamingRaven <geo.george@live.co.uk>
2018-05-29 11:13:46 +0000

    changed updater around

--- updaters/gitUpdate.py
+++ updaters/gitUpdate.py
@@ -1,8 +1,8 @@
 # @Author: George Onoufriou <georgeraven>
 # @Date:   2018-05-24
 # @Filename: gitUpdate.py
-# @Last modified by:   georgeraven
-# @Last modified time: 2018-05-24
+# @Last modified by:   archer
+# @Last modified time: 2018-05-29
 # @License: Please see LICENSE file in project root
 
 
@@ -12,18 +12,52 @@ class Gupdater(object):
     import os, sys
     from posixpath import basename, dirname
 
-    def __init__(self, urls=[], pathTo="./"):
-        self.urls = urls
 
-    def update():
+
+    def __init__(self, urls=[], path="./"):
+        self.urls = urls
+        self.path = path
+
+
+
+    def installer(path=None, urls=None):
+        urls = urls if urls is not None else self.urls
+        path = path if path is not None else self.path
+
+        # neat trick to always ensure path ends in seperator '/' by appending empty
+        path = os.path.join(path, "") # e.g "/usr/bin" vs "/usr/bin/"
+
+        for url in urls:
+            if (os.path.exists(path + os.path.basename(url)) == False):
+                print(prePend, "find=false installing:",
+                path + os.path.basename(url))
+                try:
+                    os.system("cd " + path + "; git clone " + url)
+                except:
+                    print(prePend,
+                    "Could not install:", url , "to",
+                    path + os.path.basename(url) )
+
+
+
+    def updater(path=None, urls=None):
+        urls = urls if urls is not None else self.urls
+        path = path if path is not None else self.path
+        # neat trick to force filenames to always end in seperator "/"
+        path = self.os.path.join(path, "") # e.g "/usr/bin" vs "/usr/bin/"
+
+        # attempt self update if permission availiable
         try:
-            # update self
-            os.system("git pull")
-            # update any listed dependencies
-            for url in self.urls:
-                if(os.path.exists("./" + basename(url)) == False):
-                    os.system("git clone " + url)
-                else:
-                    os.system("cd " + basename(url) +  "; git pull; cd ..")
+            print(prePend, "Updating self:")
+            self.os.system("cd " + path + "; git pull")
         except:
-            print("could not update modules", sys.exc_info()[0])
+            print(prePend + "Could not update self")
+
+            for url in urls:
+                # update any dependancies
+                print(prePend, "Updating", self.os.path.basename(url) + ":" )
+                try:
+                    os.system("cd " + path + self.os.path.basename(url) +
+                        "; git pull")
+                except:
+                    None
