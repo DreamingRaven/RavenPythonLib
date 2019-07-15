a1e55ab90c9c7241d65ea166ecc5e66614955498
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-07-15 10:53:38 +0100

    Added mongo_compat.py
    mongo_compat shall provide a python2 and 3 compatible wrapper for
    MongoDB. Initial commit for the file and base.

---
+++ mongodb/mongo_compat.py
@@ -0,0 +1,22 @@
+# @Author: George Onoufriou <archer>
+# @Date:   2019-07-15
+# @Email:  george raven community at pm dot me
+# @Filename: mongo_compat.py
+# @Last modified by:   archer
+# @Last modified time: 2019-07-15
+# @License: Please see LICENSE in project root
+
+from __future__ import print_function, absolute_import  # python 2-3 compat
+
+
+class Mongo(object):
+    """Python2/3 compatible MongoDb unility wrapper."""
+
+    def __init__(self, dictionary=None):
+        """Init class with defaults.
+
+        optionally accepts dictionary of default overides.
+        """
+        pass
+
+    __init__.__annotations__ = {"dictionary": dict, "return": None}
