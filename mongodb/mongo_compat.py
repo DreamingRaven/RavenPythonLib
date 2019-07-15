44822db96574f79f0509a60562c0156524a5bb7c
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-07-15 12:36:40 +0100

    Added python2/3 compatible dictionary merger
    private merger for merging an unknown length sequence of dictionaries

--- mongodb/mongo_compat.py
+++ mongodb/mongo_compat.py
@@ -6,25 +6,39 @@
 # @Last modified time: 2019-07-15
 # @License: Please see LICENSE in project root
 
-from __future__ import print_function, absolute_import  # python 2-3 compat
+from __future__ import print_function, absolute_import   # python 2-3 compat
 
 
 class Mongo(object):
     """Python2/3 compatible MongoDb unility wrapper."""
 
-    def __init__(self, dictionary=None):
+    def __init__(self, args=None):
         """Init class with defaults.
 
         optionally accepts dictionary of default overides.
         """
-        pass
+        args = args if args is not None else dict()
+        defaults = {
+            "test": "test",
+        }
+        self.args = self._merge_dicts(defaults, args)
+        print(self.args)
 
-    __init__.__annotations__ = {"dictionary": dict, "return": None}
+    __init__.__annotations__ = {"args": dict, "return": None}
+
+    def _merge_dicts(self, *dicts):
+        """Given multiple dictionaries, merge together in order."""
+        result = {}
+        for dictionary in dicts:
+            result.update(dictionary)  # merge each dictionary in order
+        return result
+
+    _merge_dicts.__annotations__ = {"dicts": dict, "return": None}
 
 
 def test():
     """Unit test of MongoDB compat."""
-    db = Mongo()
+    db = Mongo({"test2": 2})
 
 
 if(__name__ == "__main__"):
